337fa843dcad9610fdb64a0d32118b50f08df42a
tugascrown204 <gajdertevet@hotmail.com>
2025-08-30 08:36:53 +0330

    Add more detailed comment about DOMContentLoaded event

--- script.js
+++ script.js
@@ -1,3 +1,3 @@
-document.addEventListener('DOMContentLoaded', () => { // Log message when the DOM is fully loaded
+document.addEventListener('DOMContentLoaded', () => { // Log message when the DOM is fully loaded after all HTML is parsed
     console.log('Clean Code Guide Loaded');
 });
